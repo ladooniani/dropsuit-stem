a61c0310d1b513d1d9eb6d4623bc850705a98a47
Lado Oniani <ladooniani@gmail.com>
2023-02-23 04:28:34 +0400

    Add tests and update README for stemmer function with input data dependency

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,7 @@ let intentData = json_data.jsonIntStrct(localFile);
 
 //#region Testing data
 
-let expected_output_stem_00 = [
+let expected_output_stem1_00 = [
   "hi",
   "language",
   "for",
@@ -67,37 +67,85 @@ let expected_output_stem_11 = [
   "progra",
   "progra",
 ];
-
-let input =
+let expected_output_stem1 = ["programming"];
+let expected_output_stem2 = ["program"];
+let input1 =
   "hi, language for beginner programmers recommended languages for new programmers programming program?";
+let input2 = "programming";
 
 //#endregion
 
 describe("dropsuit-stem", () => {
   describe("stem()", () => {
-    it("should return the stemmed words array with parameter arguments 0, 0", () => {
+    it("Stemmed words array with parameter arguments 0, 0", () => {
       let dsstem = new dropsuit_stem(intentData, 0, true);
-      let stem_output = dsstem.stem(input, 0, 0, true);
-      assert.deepEqual(stem_output, expected_output_stem_00);
-      console.log("\nOutput stem(null, 0, 0):", stem_output, "\n");
+      let stem_output1 = dsstem.stem(input1, 0, 0, true);
+      let stem_output2 = dsstem.stem(input2, 0, 0, true);
+      assert.deepEqual(stem_output1, expected_output_stem1_00);
+      assert.deepEqual(stem_output2, expected_output_stem1);
+
+      //#region output additional display
+      /*
+      console.log(
+        "\nOutput stem(null, 0, 0) 1:",
+        stem_output1,
+        "\n2:",
+        stem_output2
+      );
+      */
+      //#endregion
     });
-    it("should return the stemmed words array with parameter arguments 1, 0", () => {
+    it("Stemmed words array with parameter arguments 1, 0", () => {
       let dsstem = new dropsuit_stem(intentData, 0, true);
-      let stem_output = dsstem.stem(input, 1, 0, true);
-      assert.deepEqual(stem_output, expected_output_stem_10);
-      /// console.log('\nOutput stem(null, 1, 0):',stem_output, '\n');
+      let stem_output1 = dsstem.stem(input1, 1, 0, true);
+      assert.deepEqual(stem_output1, expected_output_stem_10);
+
+      //#region output additional display
+      /*
+      console.log(
+        "\nOutput stem(null, 1, 0) 1:",
+        stem_output1,
+        "\n2:",
+        expected_output_stem2
+      );
+      */
+      //#endregion
     });
-    it("should return the stemmed words array with parameter arguments 0, 1", () => {
+    it("Stemmed words array with parameter arguments 0, 1", () => {
       let dsstem = new dropsuit_stem(intentData, 0, true);
-      let stem_output = dsstem.stem(input, 0, 1, true);
-      assert.deepEqual(stem_output, expected_output_stem_01);
-      /// console.log('\nOutput stem(null, 0, 1):',stem_output, '\n');
+      let stem_output1 = dsstem.stem(input1, 0, 1, true);
+      let stem_output2 = dsstem.stem(input2, 0, 1, true);
+      assert.deepEqual(stem_output1, expected_output_stem_01);
+      assert.deepEqual(stem_output2, expected_output_stem2);
+
+      //#region output additional display
+      /*
+      console.log(
+        "\nOutput stem(null, 0, 1):",
+        stem_output1,
+        "\n2:",
+        expected_output_stem2
+      );
+      */
+      //#endregion
     });
-    it("should return the stemmed words array with parameter arguments 1, 1", () => {
+    it("Stemmed words array with parameter arguments 1, 1", () => {
       let dsstem = new dropsuit_stem(intentData, 0, true);
-      let stem_output = dsstem.stem(input, 1, 1, true);
-      assert.deepEqual(stem_output, expected_output_stem_11);
-      /// console.log('\nOutput stem(null, 1, 1):',stem_output, '\n');
+      let stem_output1 = dsstem.stem(input1, 1, 1, true);
+      let stem_output2 = dsstem.stem(input2, 1, 1, true);
+      assert.deepEqual(stem_output1, expected_output_stem_11);
+      assert.deepEqual(stem_output2, expected_output_stem2);
+
+      //#region output additional display
+      /*
+      console.log(
+        "\nOutput stem(null, 1, 1):",
+        stem_output1,
+        "\n2:",
+        expected_output_stem2
+      );
+      */
+      //#endregion
     });
   });
 });
